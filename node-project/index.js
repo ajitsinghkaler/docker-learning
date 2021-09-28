45d1134085cb83ad0e8f9f5658f85517c68f46ef
Ajit Singh <ajitsinghkaler0@gmail.com>
2021-09-28 09:55:52 +0530

    basic project completed

--- node-project/index.js
+++ node-project/index.js
@@ -4,7 +4,7 @@ const port = 5000;
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('This is the homepage');
+    res.send('This is the homepage 1');
 });
 
 app.listen(port, () => {
